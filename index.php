30e7d7bf7360fce7753dd9cecf2695bf18128818
juoliva95 <juoliva92@users.noreply.github.com>
2021-09-14 22:55:56 -0300

    layout inicial home

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GauchoRocket</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header class="w3-container w3-theme w3-padding" id="myHeader">
+
+  <div class="w3-center">
+  <h4>GAUCHO ROCKET</h4>
+  <h1 class="w3-xxxlarge w3-animate-bottom">LOS MEJORES VIAJES ESPACIALES</h1>
+    <div class="w3-padding-32">
+      <button class="w3-btn w3-xlarge w3-dark-grey w3-hover-light-grey" onclick="document.getElementById('id01').style.display='block'" style="font-weight:900;">INGRESAR</button>
+    </div>
+  </div>
+</header>
+<div class="w3-row-padding w3-center w3-margin-top">
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>VIAJES ORBITALES</h3><br>
+  <i class="fa fa-space-shuttle w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>Chequeo médico</p>
+  <p>Hoteleria espacial</p>
+  <p>En familia</p>
+  <p>Reserva tu vuelo</p>
+  </div>
+</div>
+
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>VIAJES SUB-ORBITALES</h3><br>
+  <i class="fa fa-globe w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>Chequeo médico</p>
+  <p>Hoteleria espacial</p>
+  <p>Pago con tarjeta de credito</p>
+  <p>Reserva tu vuelo</p>
+  </div>
+</div>
+
+<div class="w3-third">
+  <div class="w3-card w3-container" style="min-height:460px">
+  <h3>TOUR</h3><br>
+  <i class="fa fa-space-shuttle w3-margin-bottom w3-text-theme" style="font-size:120px"></i>
+  <p>Chequeo médico</p>
+  <p>Hoteleria espacial</p>
+  <p>Pago con tarjeta de credito</p>
+  <p>Reserva tu vuelo</p>
+  </div>
+</div>
+</div>
+<footer class="w3-container w3-blue-grey d-flex justify-center">
+<p class="w3-opacity"></p>
+<p class="">GauchoRocket S.A. &copy; 2021</p>
+
+
+  
+</footer>
+</body>
+</html>
